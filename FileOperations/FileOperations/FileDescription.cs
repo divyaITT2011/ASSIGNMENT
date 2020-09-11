8d3956ce5d4432eca7654c3d03c77c6ac453b911
divyaITT2011 <divya.singh@intimetec.com>
2020-09-11 13:21:42 +0530

    week2 assignment

---
+++ FileOperations/FileOperations/FileDescription.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+	class FileDescription
+	{
+        public const string Path = "C:/Users/divya.singh/Desktop/demo/testFile.txt";
+
+		public void createFileAndAddText()
+		{
+			String line;
+			try
+			{
+				//Passing the file path and file name to the StreamReader constructor
+				StreamReader sr = new StreamReader(Path);
+
+				//Read the first line of text
+				line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+				{
+					//write the line to console window
+					Console.WriteLine(line);
+					//Read the next line
+					line = sr.ReadLine();
+				}
+
+				//close the file
+				sr.Close();
+				Console.ReadLine();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: " + e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Executing finally block.");
+			}
+		}
+
+		public void appendFile()
+		{
+			// Adding this text in the file
+			try
+			{
+				using (StreamWriter sw = File.AppendText(Path))
+				{
+					sw.WriteLine("This");
+					sw.WriteLine("is Extra");
+					sw.WriteLine("Text");
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: " + e.Message);
+			}
+			
+		}
+
+		public int countLines()
+		{
+			int lines = 0;
+			using (TextReader reader = File.OpenText(Path))
+			{
+				while (reader.ReadLine() != null)
+				{
+					lines++;
+				}
+			}
+			return lines;
+		}
+
+		public string lastLine()
+		{
+			var data = File.ReadAllLines(Path);
+			string last = data[data.Length - 1];
+			return last;
+		}
+
+		public bool deleteFile()
+		{
+			if (File.Exists(Path))
+			{
+				File.Delete(Path);
+				return true;
+			}
+			return false;
+			}
+
+	}
+}
---
+++ FileOperations/FileOperations/PerformingFileOperations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+	class VariousFileOperations
+	{
+	   public static void Main(String[] args)
+		{
+			FileDescription fileOperations = new FileDescription();
+			Console.WriteLine("Enter the total number of Operations to be performed ");
+			String NoOperations = Console.ReadLine();
+			int noOperations = Convert.ToInt32(NoOperations);
+			Console.WriteLine("Total Operations to be performed: " + noOperations);
+
+			while (noOperations > 0)
+			{
+				Console.WriteLine("Enter 1 to Count total number of lines");
+				Console.WriteLine("Enter 2 to Last line");
+				Console.WriteLine("Enter 3 to Append file");
+				Console.WriteLine("Enter 4 to Read text from file");
+				Console.WriteLine("Enter 5 to Delete file");
+
+				String Input = Console.ReadLine();
+				int input = Convert.ToInt32(Input);
+				switch (input)
+				{
+					case 1:
+						int counts = fileOperations.countLines();
+						Console.WriteLine("Total number of lines in file are:" + counts);
+						break;
+
+					case 2:
+						string lastL = fileOperations.lastLine();
+						Console.WriteLine("Last line of the file is:" + lastL);
+						break;
+
+					case 3:
+						Console.WriteLine("File is being appended..");
+						fileOperations.appendFile();
+						Console.WriteLine("File has been successfully appended..");
+						break;
+								
+					case 4:
+						fileOperations.createFileAndAddText();
+						Console.WriteLine("Text has been successfully read from the file..");
+						break;
+															
+					case 5:
+						bool result=fileOperations.deleteFile();
+						Console.WriteLine("File has been successfully deleted:"+result);
+						break;
+
+					default:
+						Console.WriteLine("Invalid operation..!");
+						break;
+				}
+				noOperations = noOperations - 1;
+			}
+		}
+	}
+}
